54dfa2af8bc26a3a27ce4f721ca14bd811f6e2f9
ivanlazarovg <vankata1003@gmail.com>
2019-05-28 12:07:12 +0300

    add stuff

---
+++ Duellist2 (1)/Assets/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject hand;
+    public weapon myWeapon;
+
+    private float attackTimer;
+
+    void Start()
+    {
+        myWeapon = hand.GetComponentInChildren<weapon>();
+
+    }
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+        if (Input.GetMouseButtonUp(0) && attackTimer >= myWeapon.attackCooldown)
+        {
+            attackTimer = 0f;
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, myWeapon.attackRange))
+        {
+            if(hit.collider.tag == "enemy")
+            {
+                enemyhealth health = hit.collider.GetComponent<enemyhealth>();
+                health.takeDamage(myWeapon.attackDamage);
+            }
+        }
+    }
+}
